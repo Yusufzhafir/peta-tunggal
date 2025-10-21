acbb2896f5c8a773c0d7555643ac6309200f132b
yusuf shadiq <70932237+Yusufzhafir@users.noreply.github.com>
2025-10-21 18:14:15 +0700

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import UnusedImports from "eslint-plugin-unused-imports";
+import PrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "unused-imports": UnusedImports,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+    },
+  },
+  PrettierRecommended,
   {
     ignores: [
       "node_modules/**",
